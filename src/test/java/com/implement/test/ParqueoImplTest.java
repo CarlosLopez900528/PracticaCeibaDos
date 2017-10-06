bff9a4a0fa4ece93f07383d24d4aa3b85175e6ca
CEIBA\carlos.lopez <carlos.lopez@ceiba.com.co>
2017-10-06 17:16:59 -0500

    Cambios con clase de test creada para su implementacion

---
+++ src/test/java/com/implement/test/ParqueoImplTest.java
@@ -0,0 +1,44 @@
+package com.implement.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParqueoImplTest {
+
+	@Test
+	public void testGuardarParqueo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidaClaseVehiculo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidaCantCarros() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidaCantMotos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidaDiaHabil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscarTodos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByIdClaseVehiculo() {
+		fail("Not yet implemented");
+	}
+
+}
