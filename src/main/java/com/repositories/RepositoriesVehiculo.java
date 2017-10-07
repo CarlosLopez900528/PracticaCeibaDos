efefccd8b237aca38729ef36766adf8aeee34fc9
DESKTOP-TQQ5RI6\Personal <clopez.km@gmail.com>
2017-10-06 21:28:24 -0500

    Se elimina lo que tiene que ver con clase vehiculo para que no afecte las pruebas unitarias

--- src/main/java/com/repositories/RepositoriesVehiculo.java
+++
@@ -1,9 +0,0 @@
-package com.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entities.Vehiculo;
-
-public interface RepositoriesVehiculo extends JpaRepository<Vehiculo, Long>{
-	
-}
