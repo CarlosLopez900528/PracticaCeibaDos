435df2bdd6bf68ae50466ef36702ea43b68659f4
DESKTOP-TQQ5RI6\Personal <clopez.km@gmail.com>
2017-10-08 19:46:19 -0500

    Cambios de mejora

--- src/main/java/com/implement/ParqueoImpl.java
+++ src/main/java/com/implement/ParqueoImpl.java
@@ -1,6 +1,8 @@
 package com.implement;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +20,26 @@ public class ParqueoImpl implements ParqueoService{
 	static final String VEHICULO_NO_PERMITIDO = "Este vehiculo no esta permitido o se completo el cupo";
 	static final int MAXIMO_CANT_CARRO = 20;
 	static final int MAXIMO_CANT_MOTO = 10;
+	static final int CARRO = 1;
+	static final int MOTO = 2;
 	int cantCarros;
 	int cantMotos;
+	int valorCobro;
 	boolean vehiculoValido;
 	boolean puedeCrearCarro;
 	boolean puedeCrearMoto;
 	boolean diaHabilPlaca;
+	int horaSalida;
+	int minutosSalida;
+	int segundosSalida;
 
 	@Autowired
 	private RepositoriesParqueo repoParqueo;
 	
 	public Parqueo guardarParqueo(Parqueo pq) {
 		Parqueo parq;
-		Date fechaParqueo = new Date();
+		Calendar fechaParqueo = new GregorianCalendar();
+	
 		pq.setFechaParqueo(fechaParqueo);
 		if (validaDiaHabil(pq)) {
 			if (validaClaseVehiculo(pq) && validaCantCarros(pq)) {
@@ -47,7 +56,7 @@ public class ParqueoImpl implements ParqueoService{
 	}
 	
 	public boolean validaClaseVehiculo(Parqueo pq) {		
-		if (pq.getIdClaseVehiculo() == 1 || pq.getIdClaseVehiculo() == 2) {
+		if (pq.getIdClaseVehiculo() == CARRO || pq.getIdClaseVehiculo() == MOTO) {
 			vehiculoValido = true;
 		}else {
 			vehiculoValido = false;
@@ -58,7 +67,7 @@ public class ParqueoImpl implements ParqueoService{
 	public boolean validaCantCarros(Parqueo pq) {	
 		List<Parqueo> listParqueoCa = findByIdClaseVehiculo(1);
 		cantCarros = listParqueoCa.size();
-		if (pq.getIdClaseVehiculo() == 1 && cantCarros < MAXIMO_CANT_CARRO) {
+		if (pq.getIdClaseVehiculo() == CARRO && cantCarros < MAXIMO_CANT_CARRO) {
 			puedeCrearCarro = true;
 			cantCarros = cantCarros + 1;
 		}else {
@@ -70,7 +79,7 @@ public class ParqueoImpl implements ParqueoService{
 	public boolean validaCantMotos(Parqueo pq) {
 		List<Parqueo> listParqueoMo = findByIdClaseVehiculo(2);
 		cantMotos = listParqueoMo.size();
-		if (pq.getIdClaseVehiculo() == 2 && cantMotos < MAXIMO_CANT_MOTO) {
+		if (pq.getIdClaseVehiculo() == MOTO && cantMotos < MAXIMO_CANT_MOTO) {
 			puedeCrearMoto = true;
 			cantMotos = cantMotos + 1;
 		}else {
@@ -82,7 +91,7 @@ public class ParqueoImpl implements ParqueoService{
 	public boolean validaDiaHabil(Parqueo pq) {
 		String letraInicial = pq.getPlacaVehiculo().substring(0,1);
 		if (letraInicial.equals("A")) {
-			if (pq.getFechaParqueo().getDay() == 0 || pq.getFechaParqueo().getDay() == 1) {
+			if (pq.getFechaParqueo().get(Calendar.DAY_OF_WEEK) == 0 || pq.getFechaParqueo().get(Calendar.DAY_OF_WEEK) == 1) {
 				diaHabilPlaca = true;
 			}else{
 				diaHabilPlaca = false;
@@ -92,13 +101,36 @@ public class ParqueoImpl implements ParqueoService{
 		}
 		return diaHabilPlaca;
 	}
+	
+	public int calcularCobro(Parqueo pq) {
+		if (pq.getIdClaseVehiculo() == CARRO) {
+			valorCobro = cobroCarro(pq);
+		}else{
+			valorCobro = cobroMoto(pq);
+		}
+		return valorCobro;
+	}
+	
+	public int cobroCarro(Parqueo pq) {
+		Calendar fechaSalida = new GregorianCalendar();
+		horaSalida =fechaSalida.get(Calendar.HOUR_OF_DAY);
+	    minutosSalida = fechaSalida.get(Calendar.MINUTE);
+	    segundosSalida = fechaSalida.get(Calendar.SECOND);
+		if (diaHabilPlaca) {
+			
+		}
+		return 0;
+	}
+	
+	public int cobroMoto(Parqueo pq) {
+		return 0;
+	}
 
 	public List<Parqueo> buscarTodos() {
 		return repoParqueo.findAll();
 	}
 
-	public List<Parqueo> findByIdClaseVehiculo(int idClaseVehiculo) {
-		
+	public List<Parqueo> findByIdClaseVehiculo(int idClaseVehiculo) {		
 		return repoParqueo.findByIdClaseVehiculo(idClaseVehiculo);
 	}
 }
